a9e21552b8ec8cba5673b8deacb02fd9cd57cf63
Henry29 <henry11_93@hotmail.com>
2020-06-16 17:20:27 -0500

    l

---
+++ Scripts/CodoMC.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodoMC : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mesh mesh = GetComponent<Mesh>();
+        if (mesh == null)
+            Debug.Log("Sin mesh");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void Creator()
+    {
+
+    }
+}
