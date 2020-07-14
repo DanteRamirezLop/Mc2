40d1ee2ee842ddafb2d5ff69538cfe0da387f33f
Henry29 <henry11_93@hotmail.com>
2020-07-14 01:28:57 -0500

    solo un cubo

--- Scripts/MeshControl/UnionMesh.cs
+++ Scripts/MeshControl/UnionMesh.cs
@@ -1,18 +1,47 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UnionMesh : MonoBehaviour
 {
+    private float ultancho;
+    private float ultalto;
+    private Mesh lmesh;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ultalto = 0.254f;
+        ultancho = 0.254f;
+        if (!TryGetComponent(typeof(MeshFilter), out Component c))
+        {
+            gameObject.AddComponent(typeof(MeshFilter));
+        }
+        if (!TryGetComponent(typeof(MeshRenderer), out Component d))
+        {
+            gameObject.AddComponent(typeof(MeshRenderer));
+        }
+        //GetComponent<MeshRenderer>().material = mat;
+        lmesh = GetComponent<MeshFilter>().mesh;
+        Creator();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Creator()
+    {
+        lmesh.Clear();
+        ReCrearVertices();
+        CrearTriangulos();
+        lmesh.RecalculateBounds();
+    }
+
+    private void CrearTriangulos()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void ReCrearVertices()
     {
         
     }
+    
 }
