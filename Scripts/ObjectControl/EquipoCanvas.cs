f8147df0230d9973b884d308494088f71a83006a
Boss <henry11_93@hotmail.com>
2020-07-18 23:48:04 -0500

    little send

---
+++ Scripts/ObjectControl/EquipoCanvas.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EquipoCanvas : MonoBehaviour
+{
+	public EquipoControl target;
+	public Text codigo;
+	public Dropdown tipo;
+	public Text velocidadIny;
+    public Text velocidadExt;
+    public Text porcentajeIny;
+    public Text porcentajeExt;
+    public Dropdown calculo;
+    public int vinculo;
+    public Text nivel;
+    public int idAmbiente;
+    //parte de la tabla equipoesp
+    public Text potencia;
+    public Text voltaje;
+    public Dropdown sistema;
+    public Text enfEntrada1;
+    public Text enfEntrada2;
+    public Text enfSalida1;
+    public Text enfSalida2;
+    public Dropdown tipoEsp;
+    public Text Hz;
+    public Text CSensible;
+    public Text CLatente;
+    public Text ESensible;
+    public Text ELatente;
+    public Text caudal;
+
+    
+}
